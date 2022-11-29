5c62aa46252b4950c93e5a5fcc144219d43ab428
Amira de Moya <demoyaamira@gmail.com>
2022-11-28 19:09:31 -0500

    commit tree 0a8665ac6ea75a5ca537066be122f486eadbf776

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ function add(num1, num2) {
   => null
 */
 function halve(number) {
-  if (typeof number !== "number") return null;
+  if (number !== "string") return null;
 
   return number / 2;
 }
